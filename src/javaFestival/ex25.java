111c86689a3e742ebd40c09397d6a79e5ef6921d
DongHyeok Kim <briankim0910@gmail.com>
2024-01-18 11:09:06 +0900

    Create ex25.java
    v

---
+++ src/javaFestival/ex25.java
@@ -0,0 +1,24 @@
+package javaFestival;
+
+import java.util.Scanner;
+
+public class ex25 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫자리 0을 제외한 숫자를 입력해주세요 >> ");
+		String num = sc.next();
+		String[] n = num.split("");
+		
+		int count = 0;
+		
+		for (int i = 0; i < n.length; i++) {
+			switch
+		}
+		System.out.print("대시('_')의 총 합 >> " + count);
+		
+	}
+
+}
