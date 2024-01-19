42011b0c4f53604081973885dad3b07e896ab939
DongHyeok Kim <briankim0910@gmail.com>
2024-01-19 14:13:46 +0900

    update 20

--- src/javaFestival/ex20.java
+++ src/javaFestival/ex20.java
@@ -4,14 +4,23 @@ import java.util.Scanner;
 
 public class ex20 {
     public static void main(String[] args) {
+        int n;
         Scanner sc = new Scanner(System.in);
         System.out.print("숫자 입력 >> ");
-        int decimal = sc.nextInt();
-        System.out.println(Integer.toBinaryString(decimal));
 
+        n = sc.nextInt();
+        int[] b = new int[32];
 
+        int i = 0;
+        while (n != 1) {
+            b[i++] = n % 2;
+            n = n / 2;
+        }
+        b[i] = n;
 
-
+        for (int j = i; j >= 0; --j) {
+            System.out.print(b[j]);
+        }
 
     }
 }
